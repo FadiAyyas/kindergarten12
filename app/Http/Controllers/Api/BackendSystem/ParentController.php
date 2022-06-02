45e7d7070656027aab1bee2c31189657f85c4304
fadi ayyas <fadi ayyas>
2022-06-02 19:00:23 +0300

    modifid4

--- app/Http/Controllers/Api/BackendSystem/ParentController.php
+++ app/Http/Controllers/Api/BackendSystem/ParentController.php
@@ -36,25 +36,15 @@ class ParentController extends Controller
             $Parent->email = $request->email;
             $Parent->password = Hash::make($request->password);
             $Parent->save();
-            /*
+
             foreach ($request->phoneNumbers as $phoneNumber) {
+                
                 $Phone = new ParentPhoneNumbers;
                 $Phone->type = $phoneNumber['type'];
                 $Phone->phoneNumber = $phoneNumber['phoneNumber'];
-                $Parent->phone_numbers()->save($Phone);
+                return $this->returnData('Parent Id',$phoneNumber['type'], ' Paresnts details created successfully');
+              //  $Parent->phone_numbers()->save($Phone);
             }
-            */
-            
-            $input = $request->only('phoneNumbers');
-
-            if ($request->phoneNumbers) {
-                return $this->returnData('Parent Id', $request->phoneNumbers, ' Paresnts details created successfully');
-            }
-
-            if ($input['phoneNumbers']) {
-                return $this->returnData('Parent Id', true, ' Paresnts details created successfully');
-            }
-
             return $this->returnData('Parent Id', $request->phoneNumbers, ' Paresnts details created successfully');
         } catch (Throwable $e) {
             return $this->returnError('Something was wrong, please try again late');
