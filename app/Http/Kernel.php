c8ffe65707a259a4379c754fb2f17474946e6a17
fadi ayyas <fadi ayyas>
2022-06-09 12:54:21 +0300

    Modifid10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkPasswd' => \App\Http\Middleware\checkApiPasswd::class,
-
+        'cors' => \App\Http\Middleware\Cors::class,
         'CheckToken' => \App\Http\Middleware\CheckToken::class,
         'auth.guard' => \App\Http\Middleware\AssignGuard::class,
     ];
--- routes/BackendSystemApi.php
+++ routes/BackendSystemApi.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 $backendSystem = [
     'namespace' => 'App\Http\Controllers\Api\BackendSystem',
-    'middleware' => ['checkPasswd']
+    'middleware' => ['checkPasswd','cors']
 ];
 
 Route::group($backendSystem, function () {
