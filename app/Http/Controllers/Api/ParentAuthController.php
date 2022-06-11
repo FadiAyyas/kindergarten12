d51c8782b2bd50b051110e3a2028b2774c9ae29e
fadi ayyas <fadi ayyas>
2022-06-11 21:12:10 +0300

    add parent guard

---
+++ app/Http/Controllers/Api/ParentAuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Http\Traits\GeneralTrait;
+use App\Http\Requests\Backend\AuthRequest;
+
+class ParentAuthController extends Controller
+{
+   
+}
